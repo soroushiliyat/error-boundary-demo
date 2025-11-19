1fd60b57bd8d1608d57d3eba8193521db5aba6c0
Soroush Iliyat <soroushiliyat23@gmail.com>
2025-11-19 17:10:14 +0330

    Update BuggyComponent.tsx

--- src/components/BuggyComponent.tsx
+++ src/components/BuggyComponent.tsx
@@ -1,7 +1,7 @@
 function BuggyComponent() {
     const user: any = null; // 👈 TypeScript رو خاموش می‌کنیم
 
-  return <h1>نام کاربر: {user.name}</h1>; // اینجا ارور میده چون user نال هست
+  return <h1>User name: {user.name}</h1>; // اینجا ارور میده چون user نال هست
 }
 
 export default BuggyComponent;
