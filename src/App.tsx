3bc47741123f4c5ab01272b6610837cd28947daa
Soroush Iliyat <soroushiliyat23@gmail.com>
2025-11-19 17:13:28 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SafeComponent from './components/SafeComponent';
 function App() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-      <h1>🧪 Error Handling Test in React + TypeScript</h1>
+      <h1>🧪 Error Handling Test in React + TypeScript </h1>
 
       <SafeComponent />
 
